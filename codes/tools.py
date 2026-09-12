e6b5fd5b0600172aa94f66fd3ebe314a6b6004ea
Muntaha <muntahasayed396@gmail.com>
2026-09-13 01:06:13 +0530

    tools ready

--- codes/tools.py
+++ codes/tools.py
@@ -1,8 +1,11 @@
 from dotenv import load_dotenv
 from langchain.tools import tool
 import os
+import requests
 from tavily import TavilyClient
 from bs4 import BeautifulSoup
+
+load_dotenv()
 
 client = TavilyClient(api_key=os.environ["TAVILY_API_KEY"])
 
@@ -23,3 +26,21 @@ def search_query(query:str):
         )
 
         return "\n----\n".join(results)
+
+@tool
+def scraper(url):
+    """Scrape and return clean text content from a given URL for deeper reading."""
+
+    response = requests.get(url, timeout=8, headers={"User-Agent": "Mozilla/5.0"})
+    try:
+        soup = BeautifulSoup(response.text, "html.parser")
+    
+        dcomp_tags = ["script", "style", "nav", "footer"]
+    
+        for tag in soup:
+            if tag in dcomp_tags:
+                tag.decompose
+
+        return soup.get_text(separator=" ", strip=True)[:3000]
+    except Exception as e:
+        print(f"Error in scraping \n ERROR: {e}")
