ca84ba00a1b8234e4e527b63aa0aae58f17d8fd7
Muntaha <muntahasayed396@gmail.com>
2026-09-13 13:30:42 +0530

    Agents working

---
+++ codes/app.py
@@ -0,0 +1,55 @@
+from agents import search_agent,scrape_agent,critic_chain,writer_chain
+
+def research(topic:str):
+
+    state = {}
+
+    print("Invoking search Agent")
+    
+    search_result = search_agent.invoke({"messages":[{
+    "role": "user",
+    "content": f"find latest and reliable information on {topic}"
+    }]})
+    
+    state["search_result"] = search_result["messages"][-1].content
+
+    print("Invoking scraping Agent")
+
+    scrape_result = scrape_agent.invoke({"messages":[{
+        "role":"user",
+        "content":f"""
+                    Based on the following search results about '{topic}'
+                    pick the most relevant URL and scrape it for deeper content.\n\n
+                    Search Results:\n{state['search_result'][:800]}
+        """
+    }]})
+
+    state["scrape_result"] = scrape_result["messages"][-1].content
+
+    research_string = (
+        f"""
+            Search_result : \n {state["search_result"]}\n
+            Scrape_result : \n {state["scrape_result"]}\n
+        """
+    )
+
+    print("Writing your content")
+    
+    state["report"] = writer_chain.invoke({
+        "topic":topic,
+        "research": research_string
+    })
+
+    print("Evaluating content")
+    
+    state["feedback"] = critic_chain.invoke({
+        "report":state['report']
+    })
+
+    print("\n ========================================== \n")
+
+    print("\n  Report\n",state['report'])
+    print("\n Critic \n", state['feedback'])
+
+topic = input("\n Enter a research topic : ")
+research(topic)
